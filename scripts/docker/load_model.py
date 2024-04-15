73df789c8847e5f69d639b4331536241d5cc9952
Astra Bertelli <133636879+AstraBert@users.noreply.github.com>
2024-04-15 10:24:49 +0200

    Update load_model.py

--- scripts/docker/load_model.py
+++ scripts/docker/load_model.py
@@ -1,6 +1,6 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
 
-model_checkpoint = "as-cle-bert/saccharomyces-pythia"
+model_checkpoint = "as-cle-bert/saccharomyces-pythia-v1"
 model = AutoModelForCausalLM.from_pretrained(model_checkpoint)
 tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
